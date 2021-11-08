05becc2fc1b9c7399f598d91c5b3c7bdb88e74b7
sazumy <49363119+sazumy@users.noreply.github.com>
2021-11-08 11:03:25 +0900

    とりあえずロジックが動くようにする

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,12 +1,32 @@
 class PartiesController < ApplicationController
   def join
     current_user = User.first
-    party = Party.first
+    @party = Party.first
 
+    if current_user.age < 20
+      redirect_to root_path, alert: "参加できません" and return
+    elsif current_user.missing_last_train?
+      redirect_to root_path, alert: "参加できません" and return
+    end
+
+
+    if current_user.friends.any? { |f| @party.users.include?(f) }
+      @party.users << current_user
+      redirect_to @party, notice: "ようこそ！"
+    elsif choose
+      @party.users << current_user
+      redirect_to @party, notice: "ようこそ！"
+    else
+      redirect_to root_path, notice: "参加をあきらめました"
+    end
 
   end
 
   def show
-    party = Party.find()
+    @party = Party.find(params[:id])
+  end
+
+  def choose(weight = 50)
+    rand <= weight/100.0
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   # user has_many :friends_reverse, through :reverse_friendships
   has_many :relationships, dependent: :destroy
   has_many :friends, through: :relationships, source: :friend
+
+  def missing_last_train?
+    false
+  end
 end
