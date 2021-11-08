6abaf4d433e6d0a879610b20ffd866d7f8caf367
sazumy <49363119+sazumy@users.noreply.github.com>
2021-11-08 11:34:06 +0900

    リファクタリング

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -4,22 +4,20 @@ class PartiesController < ApplicationController
     @party = Party.first
 
     if current_user.age < 20
-      redirect_to root_path, alert: "参加できません" and return
+      redirect_to root_path and return
     elsif current_user.missing_last_train?
-      redirect_to root_path, alert: "参加できません" and return
+      redirect_to root_path and return
     end
 
-
-    if current_user.friends.any? { |f| @party.users.include?(f) }
-      @party.users << current_user
+    if current_user.friends.any? { |f| @party.members.include?(f) }
+      @party.members << current_user
       redirect_to @party, notice: "ようこそ！"
     elsif choose
-      @party.users << current_user
+      @party.members << current_user
       redirect_to @party, notice: "ようこそ！"
     else
       redirect_to root_path, notice: "参加をあきらめました"
     end
-
   end
 
   def show
--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,4 @@
 class Party < ApplicationRecord
   has_many :participations, dependent: :destroy
-  has_many :users, through: :participations
+  has_many :members, through: :participations, source: :user
 end
