b0bf85396701b233d279fb188c0c2008d2e2e78d
sazumy <49363119+sazumy@users.noreply.github.com>
2021-11-08 11:33:54 +0900

    seedの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: '成人ユーザー',
+  age: 20,
+  last_train: Time.current
+)
+
+User.create!(
+  name: '未成年ユーザー',
+  age: 19,
+  last_train: Time.current
+)
+
+User.create!(
+  name: '成人ユーザーの友達',
+  age: 20,
+  last_train: Time.current
+)
+
+Party.create!(
+  name: '飲み会'
+)
